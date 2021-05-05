1e4e6e59ee0320ad2fa76092f8e45ecc9b190d58
Ken <kwill1253@gmail.com>
2021-05-04 21:00:19 -0400

    updates

--- crypto_test.go
+++ crypto_test.go
@@ -3,7 +3,6 @@ package ptcpayclient
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
-
 	"crypto/sha256"
 	"fmt"
 	"testing"
@@ -77,4 +76,58 @@ func TestCryptography_SignFail(t *testing.T) {
 	}
 }
 
+func TestCryptography_LoadKeyPair(t *testing.T) {
+	var pk = "31eb31ecf1a640cd91e0a1105501f36235f8c7d51d67dcf74ccc968d74cb6b25"
+	var cryt Cryptography
+	c := cryt.New()
+	kp := c.LoadKeyPair(pk)
 
+	kp2 := c.GenerateKeyPair()
+
+	fmt.Println("Curve:", kp.Curve)
+	fmt.Println("P256:", elliptic.P256())
+
+	fmt.Println("prv key in sign: ", kp)
+
+	msg := "hello, world"
+	// hash := sha256.Sum256([]byte(msg))
+	hash := []byte(msg)
+
+	sig, err := c.Sign(hash, kp)
+
+	nhash := sha256.Sum256([]byte(msg))
+	valid := ecdsa.VerifyASN1(&kp2.PublicKey, nhash[:], sig)
+	fmt.Println("valid: ", valid)
+	fmt.Println("err: ", err)
+	if valid {
+		t.Fail()
+	}
+
+}
+
+func TestCryptography_LoadKeyPairFail(t *testing.T) {
+	var pk = "31eb31ecf1a640cd91e0a1105501f36235f8c7d51d67dcf74ccc968d74cb6b25"
+	var cryt Cryptography
+	c := cryt.New()
+	kp := c.LoadKeyPair(pk)
+
+	fmt.Println("Curve:", kp.Curve)
+	fmt.Println("P256:", elliptic.P256())
+
+	fmt.Println("prv key in sign: ", kp)
+
+	msg := "hello, world"
+	// hash := sha256.Sum256([]byte(msg))
+	hash := []byte(msg)
+
+	sig, err := c.Sign(hash, kp)
+
+	nhash := sha256.Sum256([]byte(msg))
+	valid := ecdsa.VerifyASN1(&kp.PublicKey, nhash[:], sig)
+	fmt.Println("valid: ", valid)
+	fmt.Println("err: ", err)
+	if !valid || err != nil {
+		t.Fail()
+	}
+
+}
